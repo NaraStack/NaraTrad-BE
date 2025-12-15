a5d51a501c02678d95248bbccd84826f538bab4a
mathildahere <mathilda.here@gmail.com>
2025-12-15 19:27:26 +0700

    chore: initialize Spring Boot backend

---
+++ naratrad/src/main/java/com/naratrad/naratrad/NaratradApplication.java
@@ -0,0 +1,13 @@
+package com.naratrad.naratrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaratradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaratradApplication.class, args);
+	}
+
+}
---
+++ naratrad/src/test/java/com/naratrad/naratrad/NaratradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naratrad.naratrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaratradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
