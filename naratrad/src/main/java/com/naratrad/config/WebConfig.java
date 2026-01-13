2bd4f577c4c1696f41e78b2c496523378d2d99f4
Ammara <ammara23004@mail.unpad.ac.id>
2026-01-14 01:03:59 +0700

    Update WebConfig after FE Deployment

--- naratrad/src/main/java/com/naratrad/config/WebConfig.java
+++ naratrad/src/main/java/com/naratrad/config/WebConfig.java
@@ -10,10 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "http://nara-trad-fe.vercel.app",
+                        "https://nara-trad-fe.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
-                .maxAge(3600); // Cache preflight response for 1 hour
+                .maxAge(3600);
     }
 }
