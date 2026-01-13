9ee01c740075304819b04837a30b9f1a1ec466c7
Ammara <ammara23004@mail.unpad.ac.id>
2026-01-14 02:17:03 +0700

    Fix cors issue

--- naratrad/src/main/java/com/naratrad/config/SecurityConfig.java
+++ naratrad/src/main/java/com/naratrad/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.naratrad.config;
 import com.naratrad.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,7 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // 1. Matikan CSRF karena kita menggunakan stateless JWT
-                .cors(cors -> cors.configure(http))
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
 
                 // 2. Atur izin akses (siapa boleh akses apa)
